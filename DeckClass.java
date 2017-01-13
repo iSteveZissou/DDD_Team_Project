d8ca5076761f0ce636568b1e730e391fffc63c35
Steve Quinn <1105707q@student.gla.ac.uk>
2017-01-13 14:49:59 +0000

    added classes

---
+++ DeckClass.java
@@ -0,0 +1,69 @@
+ /** Maintains a list of CardClass objects
+ * The methods allow objects to be added and deleted from the list
+ *
+ */
+public class DeckClass {
+
+    /** Class Constants */
+
+	/** Instance Variables */
+
+	/** Default Constructor */
+
+	/** Non-Default Constructor */
+
+	/** Accessor Methods */
+
+	/** Mutator Methods */
+
+	/** Shuffles the CardClass objects on the Deck at random
+	*	@return shuffled CardClass array
+	*/
+	public CardClass [] shuffle() {
+		return null;
+	}
+
+	/** Deal the Cards in the deck to the players 
+	*   of the game
+	*/
+	public void dealCards() {
+
+	}
+
+	/** Return total number of cards on the communal pile 
+	*   @return total number of cards on the communal pile
+	*/
+	public int getDeckCount() {
+		return 0;
+	}
+
+	/** Clears the communal pile */
+	public void clearDeck() {
+
+	}
+
+	/** Adds a Card to the communal pile 
+	*	@param card to be added
+	*/
+	public void addCard(CardClass crd) {
+
+	}
+
+	/** Deletes a Card from the communal pile
+	*	@param card to be deleted
+	*
+	*	NOTE: not really necessary as the communal pile is 
+	*	either added to or cleared
+	*/
+	public void deleteCard(CardClass crd) {
+
+	}
+
+	/** Reset deack once the game is completed
+	*
+	*
+	*
+	*/
+
+
+}
