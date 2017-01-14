1d6fe416d7507a2382b5e3e041b0276e5558a346
Amy Jackson <0813273j@student.gla.ac.uk>
2017-01-14 22:34:57 +0000

    Renamed Connection class to ConnectionBuilder.

---
+++ ConnectionBuilder.java
@@ -0,0 +1,25 @@
+import java.sql.*;
+
+/**
+ * Connects to TopTrumps database to be
+ * used in EndReport and Report classes.
+ */
+
+/*
+ * Method to connect to the database
+ */
+
+public class ConnectionBuilder {
+//	//Database name
+//	static final String dbName = ""; //tbc
+//	
+//	//Database user id and password
+//	static final String username = ""; //tbc
+//	static final String password = ""; //tbc
+//	
+//	Connection c = null;
+//
+//	try/catch methods etc
+}
+//
+
