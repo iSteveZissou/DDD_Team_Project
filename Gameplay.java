283b78af7aee219b0d62000db240798ba117f6ff
Steve Quinn <1105707q@student.gla.ac.uk>
2017-01-14 20:56:12 +0000

    updated gameplay

--- Gameplay.java
+++ Gameplay.java
@@ -47,8 +47,41 @@ public class Gameplay extends JFrame{
 
 		play = new JButton("Play!");
 		JPanel pan2 = new JPanel();
-		this.add(pan2, "Center");
+		this.add(pan2, "South");
 		pan2.add(play);
+
+		JPanel pan3 = new JPanel();
+		pan3.setLayout(new BorderLayout());
+		this.add(pan3, "Center");
+
+		JPanel player1 = new JPanel();
+		JPanel player2 = new JPanel();
+		JPanel player3 = new JPanel();
+		JPanel player4 = new JPanel();
+		JPanel player5 = new JPanel();
+
+
+		pan3.add(player1, BorderLayout.NORTH);
+		pan3.add(player2, BorderLayout.CENTER);
+		pan3.add(player3, BorderLayout.WEST);
+		pan3.add(player4, BorderLayout.EAST);
+		pan3.add(player5, BorderLayout.SOUTH);
+
+		JLabel p1 = new JLabel("Player 1");
+		player1.add(p1, "East");
+
+		JLabel p2 = new JLabel("Our Card here");
+		player2.add(p2);
+
+		JLabel p3 = new JLabel("Player 3");
+		player3.add(p3);
+
+		JLabel p4 = new JLabel("Player 4");
+		player4.add(p4);
+
+		JLabel p5 = new JLabel("Player 5");
+		player5.add(p5);
+
 
 	}
 
