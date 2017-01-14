da10b1c244c65f3d673b97e34c7cacd32993c475
Steve Quinn <1105707q@student.gla.ac.uk>
2017-01-14 18:04:14 +0000

    updated report

---
+++ Report.java
@@ -0,0 +1,24 @@
+import java.awt.*;
+import javax.swing.*;
+
+/**
+ * GUI to display the Game report
+ * 
+ */
+
+public class Report extends JFrame{
+
+	/**
+	 * Contructor for the Report
+	 * parameters can be added when ready
+	 */
+	public Report(){
+		
+
+		
+	}
+
+
+
+
+}
