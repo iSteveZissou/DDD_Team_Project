245f023634f317d514bac6dc7200538bcda251b7
Steve Quinn <1105707q@student.gla.ac.uk>
2017-01-15 14:19:11 +0000

    Updated GUI

--- Gameplay.java
+++ Gameplay.java
@@ -41,50 +41,78 @@ public class Gameplay extends JFrame{
 		// This is all pretty much just placeholders for now
 		
 		JLabel welcome = new JLabel(" Welcome to Top Trumps!!");
-		welcome.setFont(new Font("Comic Sans", Font.PLAIN, 24));
+		welcome.setFont(new Font("Trebuchet MS", Font.PLAIN, 24));
 
 
 		pan = new JPanel();
 		//pan.setBackground(Color.magenta);
 		this.add(pan, "North");
 		pan.add(welcome);
+		pan.setBackground(Color.cyan);
 
-		play = new JButton("Play!");
-		JPanel pan2 = new JPanel();
-		this.add(pan2, "South");
-		pan2.add(play);
+		GridLayout grid = new GridLayout(5, 2);
+		JPanel pan3 = new JPanel(grid);
+		this.add(pan3, "West");
 
-		JPanel pan3 = new JPanel();
-		pan3.setLayout(new BorderLayout());
-		this.add(pan3, "Center");
-
-		JPanel player1 = new JPanel();
-		JPanel player2 = new JPanel();
-		JPanel player3 = new JPanel();
-		JPanel player4 = new JPanel();
-		JPanel player5 = new JPanel();
+		GridLayout grid2 = new GridLayout(5, 2);
+		
+		
+		JPanel eastPan = new JPanel(grid2);
+		this.add (eastPan, "East");
+		
+		
+		JLabel lab = new JLabel("Communal pile: ");
 
 
-		pan3.add(player1, BorderLayout.NORTH);
-		pan3.add(player2, BorderLayout.CENTER);
-		pan3.add(player3, BorderLayout.WEST);
-		pan3.add(player4, BorderLayout.EAST);
-		pan3.add(player5, BorderLayout.SOUTH);
+		JPanel centerPan = new JPanel();
+		this.add(centerPan, "Center");
+		centerPan.add(lab);
 
-		JLabel p1 = new JLabel("Player 1");
-		player1.add(p1, "East");
+		JLabel p1 = new JLabel("User");
+		//pan3.add(p1);
 
-		JLabel p2 = new JLabel("Our Card here");
-		player2.add(p2);
+		JLabel p2 = new JLabel("Player 2");
+		pan3.add(p2);
+		JLabel score2 = new JLabel("Score: ");
+		JLabel score3 = new JLabel("Score: ");
+		JLabel score4 = new JLabel("Score: ");
+		JLabel score5 = new JLabel("Score: ");
+		pan3.add(score2);
 
 		JLabel p3 = new JLabel("Player 3");
-		player3.add(p3);
+		eastPan.add(p3);
+		eastPan.add(score3);
 
 		JLabel p4 = new JLabel("Player 4");
-		player4.add(p4);
+		pan3.add(p4);
+		pan3.add(score4);
 
 		JLabel p5 = new JLabel("Player 5");
-		player5.add(p5);
+		eastPan.add(p5);
+		eastPan.add(score5);
+
+		// for testing
+		String descrip = "T-Rex";
+
+		//Bottom panel
+		play = new JButton("Play!");
+		JPanel pan2 = new JPanel();
+		JLabel cat = new JLabel("Categories:");
+		JLabel topCard = new JLabel("Top Card:");
+		JTextField card = new JTextField(descrip.length());
+		card.setText(descrip);
+		card.setEditable(false);
+
+		this.add(pan2, "South");
+				pan2.add(topCard);
+		pan2.add(card);
+		pan2.add(cat);
+
+		String [] testString = {"height: 9", "weight: 5", "length: 7"};
+		JComboBox<String> jcom = new JComboBox<String>(testString);
+		pan2.add(jcom);
+		pan2.add(play);
+		pan2.setBackground(Color.cyan);
 
 
 	}
