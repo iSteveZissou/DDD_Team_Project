5320065c619609f78041a3bab33df4197c13172c
Steve Quinn <1105707q@student.gla.ac.uk>
2017-01-16 21:54:38 +0000

    GUI Update

--- TopTrumpsGUI.java
+++ TopTrumpsGUI.java
@@ -19,7 +19,7 @@ public class TopTrumpsGUI extends JFrame implements ActionListener{
 	 */
 	public TopTrumpsGUI(){
 
-		this.setSize(300, 150);
+		this.setSize(300, 170);
 		this.setLocation(100,100);
 		this.setTitle ("Top Trumps!");
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -35,29 +35,37 @@ public class TopTrumpsGUI extends JFrame implements ActionListener{
 	 */
 	public void layOut(){
 
+		GridLayout grid = new GridLayout(4, 1);
+
+		JLabel welcome = new JLabel("Top Trumps!!");
+		welcome.setFont(new Font("Courier", Font.PLAIN, 28));
+		welcome.setHorizontalAlignment(JLabel.CENTER);
+
+
 	
 		JPanel pan = new JPanel();
 		JPanel pan2 = new JPanel();
 		JPanel pan3 = new JPanel();
 
-		
-
-
+		pan.setPreferredSize(new Dimension(70, 70));
 		newGame = new JButton("NEW GAME");
 		newGame.addActionListener(this);
+		//newGame.setPreferredSize(new Dimension(10, 10));
+
 		reportButton = new JButton("REPORT");
 		reportButton.addActionListener(this);
 		exitButton = new JButton("EXIT");
 		exitButton.addActionListener(this);
 
 		this.add(pan, "North");
-		pan.setBackground(Color.magenta);
+		pan.setBackground(Color.cyan);
 		this.add(pan2, "Center");
-		pan2.setBackground(Color.magenta);
+		pan2.setBackground(Color.cyan);
 		this.add(pan3, "South");
-		pan3.setBackground(Color.magenta);
+		pan3.setBackground(Color.cyan);
 
 		
+		pan.add(welcome);
 		pan.add(newGame);
 		pan2.add(reportButton);
 		pan3.add(exitButton);
@@ -95,7 +103,7 @@ public class TopTrumpsGUI extends JFrame implements ActionListener{
 		if (e.getSource()==newGame){
 			//start new Game
 			this.startGame();
-			this.setVisible(false);
+			//this.setVisible(false);
 			System.out.println("New Game Begins");
 		}
 
