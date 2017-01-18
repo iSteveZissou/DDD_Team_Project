3e072a8c87934a29a004f6c16d256b2950bd2a60
themicrogirl <e.grabowiecka@outlook.com>
2017-01-18 18:00:57 +0000

    Added connectivity

--- SQLRead.java
+++ SQLRead.java
@@ -1,4 +1,25 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class SQLRead {
-	//for the main Report 
+	
+	private Connection connection = null;
+	
+	public  SQLRead(){
+		
+		String DBname = "m_16_2246923g";
+		String username = "m_16_2246923g";
+		String pass = "2246923g"; 
+
+		try {
+			connection = DriverManager.getConnection("jdbc:postgresql://yacata.dcs.gla.ac.uk:5432/" + DBname, username,	pass);
+		}
+
+		catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+	}
+	
 }
--- SQLWrite.java
+++ SQLWrite.java
@@ -1,4 +1,30 @@
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 
 public class SQLWrite {
+	
+	private Connection connection = null;
+	
+	public  SQLWrite(){
+	
+		DBConnect();
+	}
+			
+	public void DBConnect() {
+	
+		
+		String DBname = "m_16_2246923g";
+		String username = "m_16_2246923g";
+		String pass = "2246923g"; 
 
+		try {
+			connection = DriverManager.getConnection("jdbc:postgresql://yacata.dcs.gla.ac.uk:5432/" + DBname, username,	pass);
+		}
+
+		catch (SQLException e) {
+			e.printStackTrace();
+		}
+
+	}
 }
