2d7ab5c1b9b1442e078508e00f3cb34fd68e14aa
Assandra Falls <Assandra Falls>
2017-02-08 15:11:51 +0000

    Refactor and Comments done

--- CardClass.java
+++ CardClass.java
@@ -1,6 +1,6 @@
 /** Defines an object representing a single card
  */
-public class CardClass implements Comparable<CardClass> {
+public class CardClass {
 
 	/** Class Constants */
 	private static final int NUM_CATEGORIES = 5;
@@ -8,19 +8,9 @@ public class CardClass implements Comparable<CardClass> {
 	/** Instance Variables */
 	private String description;
 
-	private int cat1;
-
-	private int cat2;
-
-	private int cat3;
-	
-	private int cat4;
-
-	private int cat5;
+	private int cat1, cat2, cat3, cat4, cat5;
 
 	private int [] cardValues;
-
-
 
 	/** Default Constructor */
 	public CardClass() {
@@ -28,9 +18,8 @@ public class CardClass implements Comparable<CardClass> {
 	
 	/** Non-default constructor */
 	public CardClass(String cardInfo) {
-		String info = cardInfo;
 		
-		String [] tokens = info.split(" ");
+		String [] tokens = cardInfo.split(" ");
 		
 		for (int i=0; i<tokens.length; i++)
 		{
@@ -42,7 +31,9 @@ public class CardClass implements Comparable<CardClass> {
 			cat5 = Integer.parseInt(tokens[5]);	
 		}
 
-		cardValues = new int [6];
+		cardValues = new int [NUM_CATEGORIES+1];
+
+		//The cardValues indices correspond to the category numbering
 		cardValues[0] = 0;
 		cardValues[1] = cat1;
 		cardValues[2] = cat2;
@@ -57,77 +48,48 @@ public class CardClass implements Comparable<CardClass> {
 		return description;
 	}
 
-	/** Accessor for height */
+	/** Accessor for cat1 */
 	public int getCatOne() {
 		return cat1;
 	}
 
-	/** Accessor for weight */
+	/** Accessor for cat2 */
 	public int getCatTwo() {
 		return cat2;
 	}
 
-	/** Accessor for length */
+	/** Accessor for cat3 */
 	public int getCatThree() {
 		return cat3;
 	}
 
-	/** Accessor for ferocity */
+	/** Accessor for cat4 */
 	public int getCatFour() {
 		return cat4;
 	}
 
-	/** Accessor for intelligence */
+	/** Accessor for cat4 */
 	public int getCatFive() {
 		return cat5;
 	}
 
-
 	/**
-	*	Converts string name of a category to index in the category array
-	*	@param String name of category
-	*	@return index value
-	*/
-	public int getCategory(String category) {
-		return 0;
-	}
-
-	/** Compares values of each category 
-	*	a card
-	*	@return name of the category	 
-	*/ 
-	public String compareCategory() {
-		return "";
-	}
-
-	/** Compare two CardClass objects
-	*	@param other CardClass object to compare against
-	*	@return 
-	*/
-	public int compareTo(CardClass other) {
-	  	return 0; 
-    }
-
-    /**
-    * Returns a string representation of the card, including both
-    * the description and the values.
-    * Example: "TRex 6 6 12 9 9"
-    * @return card details 
-    */
-  	public String toString() {
-   		return description +" " +cat1 +" "+ cat2 +" "+ cat3 +" "+ cat4 +" " +cat5;
-	}  
-
-	/**
-	 *  returns the value at a given index for the user's topcard
-	 * @param  n [the index 
-	 * @return   the value at that index
+	 * Returns the value at a given index which indicates the category
+	 * for the user's topcard.
+	 * @param  indexCat - the index of the category 
+	 * @return  the value at that index
 	 */
-	public int catAtIndex(int n){
-
-		return cardValues[n];
+	public int catAtIndex(int indexCat){
+		return cardValues[indexCat];
 	}
 
+	/**
+	 * Method which finds the highest category value for a 
+	 * given card.
+	 * This method uses the find the maxinum value of an array algorithm.
+	 * @return the index of the category with the highest value.
+	 */
+	 
 	public int getHighestValue() {
 
 		int largest = cardValues[0];
@@ -139,13 +101,19 @@ public class CardClass implements Comparable<CardClass> {
 				largest = cardValues[i];
 				largestIndex=i;
 			}
-			i++;
-			
-			
+				i++;		
 		}
-		System.out.println("The category index choosen is " + largestIndex);
-
 		return largestIndex;
 	}
+
+    /**
+    * Returns a string representation of the card, including both
+    * the description and the values.
+    * Example: "TRex 6 6 12 9 9"
+    * @return card details as a String
+    */
+  	public String toString() {
+   		return description +" " +cat1 +" "+ cat2 +" "+ cat3 +" "+ cat4 +" " +cat5;
+	}  
 
 }
